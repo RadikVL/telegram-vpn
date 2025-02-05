8b3c9ce6d82ca412c41728a32297a962eeb3b4b2
Raddik <129759639+RadikVL@users.noreply.github.com>
2025-02-05 17:59:18 +0300

    alpha0.0.4

--- main/handlers/user_handlers.py
+++ main/handlers/user_handlers.py
@@ -8,12 +8,12 @@ router = Router()
 
 @router.message(CommandStart())
 async def cmd_start(message: Message):
-    await message.reply('/main page/', reply_markup=kb.main)
+    await message.reply('*main page*', reply_markup=kb.main)
 
 @router.callback_query(F.data == 'mainpage')
 async def main_page(callback: CallbackQuery):
     await callback.answer('')
-    await callback.message.edit_text(text='/main page/', reply_markup=kb.main)
+    await callback.message.edit_text(text='*main page*', reply_markup=kb.main)
 
 # callback-handlers for 'main' keyboard
 @router.callback_query(F.data == 'connection')
@@ -35,3 +35,4 @@ async def settings(callback: CallbackQuery):
 async def status(callback: CallbackQuery):
     await callback.answer('')
     await callback.message.edit_text(text='*connection status - etc.*', reply_markup=kb.main)
+    
